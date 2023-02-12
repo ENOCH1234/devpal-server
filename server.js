d37845ad7d6433b4ba5a15ffef97668afc696113
unknown <ezekielabolade@gmail.com>
2023-02-12 13:57:26 +0100

    API hidden

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ import { Configuration, OpenAIApi } from "openai";
 dotenv.config();
 
 const configuration = new Configuration({
-  // apiKey: process.env.OPENAI_API_KEY
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
