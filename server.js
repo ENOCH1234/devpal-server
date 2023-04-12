b692d613652000d2652bee03bdff9b5d5e61dc6b
unknown <ezekielabolade@gmail.com>
2023-04-12 19:16:00 +0100

    Slight update

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/", async (req, res) => {
   });
 });
 
-app.post("/webhook", (req, res) => {
+app.post("/webhooks", (req, res) => {
   if (
     req.query["hub.mode"] == "subscribe" &&
     req.query["hub.verify_token"] == "THEFACE"
