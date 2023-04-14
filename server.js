76236cee9ae4035143f5104b6c35c5ed84e010bf
unknown <ezekielabolade@gmail.com>
2023-04-14 03:34:27 +0100

    Slight update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
-import WhatsApp from "facebook-nodejs-business-sdk";
+const { WhatsApp } = require("facebook-nodejs-business-sdk");
 
 dotenv.config();
 
