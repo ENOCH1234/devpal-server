940a0bb0f28af9f0c08a2603c5f8320bad24f90a
unknown <ezekielabolade@gmail.com>
2023-04-19 13:10:18 +0100

    Slight update

--- server.js
+++ server.js
@@ -157,6 +157,7 @@ app.post("/webhooks", async (req, res) => {
     case "audio":
       try {
         const audioFilePath = await getAudioURL(message.audio);
+        console.log("Path", audioFilePath);
         const audioFile = fs.writeFileSync("C:\\", audioFilePath);
         console.log("Audio", audioFile);
         const transcriptionOptions = {
